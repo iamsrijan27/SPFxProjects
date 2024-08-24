bd963732fa19d5275ea7ad3600d4689140888eae
iamsrijan27 <srijanjha1999@gmail.com>
2024-08-24 20:14:36 +0530

    ui change

---
+++ POCReactApp/youtube-clone/src/Components/Feed/Feeds.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Feed.css';
+import thumbnail1 from '../../assets/thumbnail1.png'
+const Feeds =()=>{
+    return (
+        <div className='card'>
+            <img src={thumbnail1} alt=''/>
+            <h2>Best channel to learn coding that help ou to be a web developer</h2>
+            <h3></h3>
+        </div>
+    )
+}
+
+export default Feeds
--- POCReactApp/youtube-clone/src/Pages/Home/Home.jsx
+++ POCReactApp/youtube-clone/src/Pages/Home/Home.jsx
@@ -3,10 +3,12 @@
 import React from 'react'
 import './Home.css';
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import Feeds from '../../Components/Feed/Feeds';
 const Home = ({sidebar}) => {
   return (
     <>
     <Sidebar sidebar={sidebar}/>
+    <div className={`container ${sidebar?"":'large-container'}`}><Feeds/></div>
     </>
   )
 }
